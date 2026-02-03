a87c1f73ec7cd96ecc95d25416354f37c7ab8ccc
capoia <weellingtonc@gmail.com>
2026-02-03 09:20:34 -0300

    Simulate vue type errors on PostsController (#1)
    Co-authored-by: weellingtonc <weellintonc@gmail.com>

--- .adonisjs/server/controllers.ts
+++ .adonisjs/server/controllers.ts
@@ -1,6 +1,6 @@
 export const controllers = {
-  NewAccount: () => import('#controllers/new_account_controller'),
-  Session: () => import('#controllers/session_controller'),
-  Posts: () => import('#controllers/posts_controller'),
   Comments: () => import('#controllers/comments_controller'),
+  NewAccount: () => import('#controllers/new_account_controller'),
+  Posts: () => import('#controllers/posts_controller'),
+  Session: () => import('#controllers/session_controller'),
 }
--- inertia/types.ts
+++ inertia/types.ts
@@ -2,3 +2,7 @@ import { type Data } from '@generated/data'
 import { type JSONDataTypes } from '@adonisjs/core/types/transformers'
 
 export type InertiaProps<T extends JSONDataTypes = {}> = Data.SharedProps & T
+
+export type User = Data.User
+export type Post = Data.Post
+export type Comment = Data.Comment
